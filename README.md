7abe54611b0b8170fa845bf5ceb7d39692aab1d7
oscar.franco <ofrubio0@gmail.com>
2023-04-04 09:32:16 -0500

    Inital Commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# google-translate-clone
+# Google Translate Clone
 Este es un clon de la herramienta de traducción de Google, construido con Vite + React + TypeScript y utilizando la API de ChatGPT para la traducción de texto.
+
+![](https://media.gcflearnfree.org/content/57a0a2ce8d7fb205008d1d07_08_02_2016/googletips_translate_blank.jpg)
+
+## Instalación
+Para instalar y ejecutar el proyecto en tu máquina local, sigue los siguientes pasos:
+
+1. Clona el repositorio a tu máquina local:
+ ```bash
+   $ https://github.com/DevRubio/google-translate-clone.git
+   ```
+
+2. Instala las dependencias del proyecto:
+```bash
+npm install
+```
+3. ingresa la ApiKey de ChatGPT en el archivo .env
+
+4. Inicia la aplicación en modo de desarrollo:
+```bash
+npm run dev
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,104 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
-import { useStore } from './hooks/useStore';
-import { Container, Row, Col } from 'react-bootstrap';
-
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { useStore } from "./hooks/useStore";
+import { Container, Row, Col, Button, Form, Stack } from "react-bootstrap";
+import { useDebounce } from "./hooks/useDebounce";
+import { AUTO_LANGUAGE } from "./constants";
+import { ArrowsIcon, ClipboardIcon } from "./components/Icons";
+import { LanguageSelector } from "./components/LanguageSelector";
+import { SectionType } from "./types.d";
+import { TextArea } from "./components/TextArea";
+import { useEffect } from "react";
+import { translate } from "./services/translate";
 
 function App() {
- const { fromLanguage, setFromLanguage } = useStore()
+  const {
+    loading,
+    fromLanguage,
+    toLanguage,
+    fromText,
+    result,
+    interchangeLanguages,
+    setFromLanguage,
+    setToLanguage,
+    setFromText,
+    setResult,
+  } = useStore();
+
+  const debouncedFromText = useDebounce(fromText, 300);
+
+  useEffect(() => {
+    if (debouncedFromText === "") return;
+
+    translate({ fromLanguage, toLanguage, text: debouncedFromText })
+      .then((result) => {
+        if (result == null) return;
+        setResult(result);
+      })
+      .catch(() => {
+        setResult("Error");
+      });
+  }, [debouncedFromText, fromLanguage, toLanguage]);
+
+  const handleClipboard = () => {
+    navigator.clipboard.writeText(result).catch(() => {})
+  }
+
   return (
     <Container fluid>
-      <h1>Google Transalate</h1>
+      <h2>Google Translate</h2>
       <Row>
         <Col>
-        <h2>From</h2>
+          <Stack gap={2}>
+            <LanguageSelector
+              type={SectionType.From}
+              value={fromLanguage}
+              onChange={setFromLanguage}
+            />
+            <TextArea
+              type={SectionType.From}
+              value={fromText}
+              onChange={setFromText}
+            />
+          </Stack>
+        </Col>
+        <Col xs="auto">
+          <Button
+            variant="link"
+            disabled={fromLanguage === AUTO_LANGUAGE}
+            onClick={interchangeLanguages}
+          >
+            <ArrowsIcon />
+          </Button>
         </Col>
         <Col>
-        <button></button>
-        </Col>
-        <Col>
-        <h2>to</h2>
+          <Stack gap={2}>
+            <LanguageSelector
+              type={SectionType.To}
+              value={toLanguage}
+              onChange={setToLanguage}
+            />
+            <div style={{ position: 'relative' }}>
+            <TextArea
+              loading={loading}
+              type={SectionType.To}
+              value={result}
+              onChange={setResult}
+            />
+            <div style={{ position: 'absolute', left: 0, bottom: 0, display: 'flex' }}>
+            <Button
+              variant='link'
+              onClick={handleClipboard}>
+                <ClipboardIcon />
+            </Button>            
+            </div>
+
+            </div>
+          </Stack>
         </Col>
       </Row>
-      <button onClick={()=>{
-        setFromLanguage('esdsdsdsd')
-      }}>Cambiar a Español</button>
-      {fromLanguage}
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
